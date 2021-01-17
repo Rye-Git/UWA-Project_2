5a119f69661d2d3f8c4eb11199bf143d9fb0fcd6
CicilyGeorge <69083251+CicilyGeorge@users.noreply.github.com>
2021-01-18 00:21:21 +0800

    More API endpoints

--- source_code/app.py
+++ source_code/app.py
@@ -14,6 +14,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/populationDB")
 
 # Flask Routes
 
+# json API pages
 @app.route('/api/population/latest')
 def latest():
     latest = mongo.db.latestPopulation.find() # PYMONGO CURSOR 
@@ -21,14 +22,12 @@ def latest():
     json_latest = json.loads(dump_latest) # CONVERT TO ACTUAL JSON 
     return jsonify(json_latest)  # RETURN JSON 
 
-
-
 @app.route("/api/population/countries")
 def countries():
-    countries = mongo.db.countriesPopulation.find()
-    dump_countries = dumps(countries)
-    json_countries = json.loads(dump_countries)
-    return jsonify(json_countries) 
+    countries = mongo.db.countriesPopulation.find() # PYMONGO CURSOR 
+    dump_countries = dumps(countries) # CONVERT PYMONGO CUSTOR TO JSON STRING 
+    json_countries = json.loads(dump_countries) # CONVERT TO ACTUAL JSON 
+    return jsonify(json_countries)   # RETURN JSON 
 
 @app.route('/api/population/cities')
 def cities():
@@ -37,8 +36,43 @@ def cities():
     json_cities = json.loads(dump_cities)
     return jsonify(json_cities) 
 
+@app.route('/api/population/world')
+def world():
+    data = mongo.db.worldPopulation.find()
+    dump_data = dumps(data)
+    json_data = json.loads(dump_data)
+    return jsonify(json_data) 
+
+@app.route('/api/population/density')
+def density():
+    data = mongo.db.populationDensity.find()
+    dump_data = dumps(data)
+    json_data = json.loads(dump_data)
+    return jsonify(json_data) 
+
+@app.route('/api/population/birth')
+def birth():
+    data = mongo.db.birthRate.find()
+    dump_data = dumps(data)
+    json_data = json.loads(dump_data)
+    return jsonify(json_data) 
+
+@app.route('/api/population/death')
+def death():
+    data = mongo.db.deathRate.find()
+    dump_data = dumps(data)
+    json_data = json.loads(dump_data)
+    return jsonify(json_data) 
+
+@app.route('/api/population/sex-ratio')
+def sexRatio():
+    data = mongo.db.sexRatio.find()
+    dump_data = dumps(data)
+    json_data = json.loads(dump_data)
+    return jsonify(json_data) 
 
 
+# html pages
 @app.route('/')
 def index():
     return render_template('index.html')
